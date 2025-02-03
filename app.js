d62d326f0ff2e94d40b1087fb800f7a9e2d38a57
Shariq Choudhary <khanshariq92213@gmail.com>
2025-02-03 22:49:28 +0530

    Minor Changes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import userRouter from "./routes/user.js";
 import apiKeyRouter from "./routes/apiKey.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 export const app = express();
 
 
