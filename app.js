8791cd4f2e5580286e231a04744c649aa86227b2
Shariq Choudhary <khanshariq92213@gmail.com>
2025-02-05 22:35:59 +0530

    Minor changes

--- app.js
+++ app.js
@@ -3,24 +3,31 @@ import userRouter from "./routes/user.js";
 import apiKeyRouter from "./routes/apiKey.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables
+
+
 export const app = express();
 
-
 // Middleware
-app.use(express.json()); // This will parse the incoming request body to json
-app.use("/api/v2/users", userRouter);
-app.use("/api/v2/apiKey", apiKeyRouter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: ["http://localhost:3000", process.env.FRONTEND_URL_1,process.env.FRONTEND_URL_2,"http://localhost:5000","http://localhost:8000"],
+app.use(cors({
+    origin: [
+        "http://localhost:3000",
+        process.env.FRONTEND_URL_1,
+        process.env.FRONTEND_URL_2,
+        "http://localhost:5000",
+        "http://localhost:8000"
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-  }),
-);
+}));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+
+// Use Routers
+app.use("/api/v2/users", userRouter);
+app.use("/api/v2/apiKey", apiKeyRouter);
+
